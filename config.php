f770cd02b6d372b2dc77400ebd65fc5569b57dea
grigorylushov <iphonephotosalexandra@gmail.com>
2025-04-09 19:41:54 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
-define('DBSERVER', 'localhost'); // Database server
-define('DBUSERNAME', 'root'); // Database username
-define('DBPASSWORD', ''); // Database password
-define('DBNAME', 'demo'); // Database name
+define('DBSERVER', 'b97824gy.beget.tech'); // Database server
+define('DBUSERNAME', 'b97824gy_account'); // Database username
+define('DBPASSWORD', 'Account123!'); // Database password
+define('DBNAME', 'b97824gy_account'); // Database name
  
 /* connect to MySQL database */
 $db = mysqli_connect(DBSERVER, DBUSERNAME, DBPASSWORD, DBNAME);
