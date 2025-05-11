2fd64194bc4fbea6f32645dd1a1abc1527a03e99
grigorylushov <iphonephotosalexandra@gmail.com>
2025-05-11 18:44:38 +0300

    Add files via upload

--- repo.sh
+++ repo.sh
@@ -1,10 +1,11 @@
 echo welcome to charge repo for KALI for Russian PC
 echo starting sudo...
 sudo echo done!
-wget http://grigorylushov.github.io/kalisources.list -o sources.list
+wget http://grigorylushov.github.io/sources.list
 sudo rm /etc/apt/sources.list 
 sudo cp sources.list /etc/apt
 echo Finishing...
 rm sources.list
+sudo apt update -y
 echo done!
 exit
