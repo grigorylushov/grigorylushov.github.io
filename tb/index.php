9cadf06361cd1ebb9a09fdce86cc957df8891821
grigorylushov <iphonephotosalexandra@gmail.com>
2026-04-30 20:23:47 +0300

    Update index.php

--- tb/index.php
+++ tb/index.php
@@ -1,6 +1,6 @@
 <?php
-print("Please Wait! Downloading the latest version on Russian")
+print("Please Wait! Downloading the latest version on Russian");
 sleep(5);
-header(“Location: https://grigorylushov.github.io/tb/1.2/happybirthday.apk”);
-exit( );
+header("Location: https://grigorylushov.github.io/tb/1.2/happybirthday.apk");
+exit();
 ?>
